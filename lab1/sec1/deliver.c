952f792e742fc98201db046250dfd90797ec4324
Andreas Avramidis <andreas.avramidis@mail.utoronto.ca>
2022-01-21 17:05:14 -0500

    Added \n to final print statement

--- lab1/sec1/deliver.c
+++ lab1/sec1/deliver.c
@@ -71,7 +71,7 @@ int main(int argc, char *argv[]){
         exit(1);
     }
 
-    printf("A file transfer can start.");
+    printf("A file transfer can start.\n");
     close(socketFD);
     return 0;
 }
