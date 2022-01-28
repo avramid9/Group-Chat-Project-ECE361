24571d5ec57519e7ca710dfb52829c0d1d5191fb
James Yen <james.yen@mail.utoronto.ca>
2022-01-27 20:45:27 -0500

    wrote time stuff, wrote some of deliver, not sure how to parse strings not using string functions though

--- lab1/sec2/sec2.2/deliver.c
+++ lab1/sec2/sec2.2/deliver.c
@@ -6,6 +6,23 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+struct packet {  
+    unsigned int total_frag;  
+    unsigned int frag_no; 
+    unsigned int size; 
+    char* filename; 
+    char filedata[1000];  
+};
+
+char* packet_to_single(struct packet p){
+    //idk how to concatenate without using string functions ree
+    char* k = p.total_frag + ":" + p.frag_no + ":" + p.size + ":" + p.filename + ":" + p.filedata;
+    return k;
+}
 
 
 int main(int argc, char *argv[]){
@@ -39,6 +56,40 @@ int main(int argc, char *argv[]){
         exit(1);
     }
     
+    //read file + create packets/1000 byte
+    //check file size first
+    struct stat st;
+    stat(fileLoc, &st);
+    int size = st.st_size;
+    
+    int total_frag = st.st_size/1000+1;
+    int frag_no=1;
+    
+    struct packet packet_array[100]; //used to store packets we need to send
+    char temp_data[1000];
+    
+    int fd_from;
+    
+    ssize_t reader;
+    if((fd_from=open(fileLoc, O_RDONLY))<0)
+        printf("error reading file");   
+    while((reader=read(fd_from,temp_data,sizeof(temp_data)))>0){ 
+        packet_array[frag_no].total_frag = total_frag;
+        packet_array[frag_no].frag_no = frag_no;
+        packet_array[frag_no].filename = fileLoc;
+        packet_array[frag_no].size = size;
+        packet_array[frag_no].filedata = temp_data;
+        
+        //increment fragment number
+        frag_no++;
+    }
+    
+    //close file since we're done reading
+    if(close(fd_from)<0)
+        printf("error closing file");
+    
+    
+    
     //Put server information into sockaddr_in
     struct sockaddr_in sa;
     memset(&sa.sin_zero, 0, 8*sizeof(unsigned char));
@@ -47,12 +98,17 @@ int main(int argc, char *argv[]){
     inet_pton(AF_INET, ipAddress, &(sa.sin_addr));
 
     ssize_t bytesSent;
+    
+    //continue ...
+    
+    /*
     //Send ftp message to server
     if((bytesSent = sendto(socketFD, "ftp", sizeof("ftp"), 0, (struct sockaddr *) &sa, sizeof(sa))) == -1){
         printf("Error sending message to server\n");
         exit(1);
     }
-
+    
+    
 
     struct sockaddr_storage sa_stor;
     socklen_t sa_stor_size = sizeof(sa_stor);
@@ -66,7 +122,8 @@ int main(int argc, char *argv[]){
         printf("Error receiving message from server");
         exit(1);
     }
-
+    
+    
     //Check return message from server
     if(strcmp(buf, "yes") != 0){
         exit(1);
@@ -74,5 +131,6 @@ int main(int argc, char *argv[]){
 
     printf("A file transfer can start.\n");
     close(socketFD);
+     */
     return 0;
 }
