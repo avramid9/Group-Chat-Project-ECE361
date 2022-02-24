2701e763d3741b08ea02f1269254ba50691a59b2
James Yen <james.yen@mail.utoronto.ca>
2022-02-23 20:47:15 -0500

    set up lab3 directory and files.

---
+++ lab1/sec3/deliver.c
@@ -0,0 +1,211 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+struct packet {  
+    unsigned int total_frag;  
+    unsigned int frag_no; 
+    unsigned int size; 
+    char* filename; 
+    char filedata[1000];  
+};
+
+char* packet_to_message(struct packet p, int* message_len){
+    //Get strings and their length from first 3 fields in packet
+    int total_frag_len = snprintf(NULL, 0, "%d", p.total_frag);
+    char* total_frag = malloc(total_frag_len + 1);
+    snprintf(total_frag, total_frag_len+1, "%d", p.total_frag);
+
+    int frag_no_len = snprintf(NULL, 0, "%d", p.frag_no);
+    char* frag_no = malloc(frag_no_len + 1);
+    snprintf(frag_no, frag_no_len+1, "%d", p.frag_no);
+
+    int size_len = snprintf(NULL, 0, "%d", p.size);
+    char* size = malloc(size_len + 1);
+    snprintf(size, size_len+1, "%d", p.size);
+    
+    //Allocate char array for the message which will be returned as a pointer
+    int mSize = 4 + total_frag_len + frag_no_len + size_len + strlen(p.filename) + p.size; 
+    char *m = malloc(mSize);
+    char test = (char) p.total_frag;
+
+    //Keep track of position each time we add a string to the message
+    int mPos = 0;
+
+    //Add first three variables to message
+    for(int i = mPos; i < (mPos + total_frag_len); i++){
+        m[i] = total_frag[i-mPos];
+    }
+    m[mPos + total_frag_len] = ':';
+    mPos += total_frag_len+1;
+
+    for(int i = mPos; i < (mPos + frag_no_len); i++){
+        m[i] = frag_no[i-mPos];
+    }
+    m[mPos + frag_no_len] = ':';
+    mPos += frag_no_len+1;
+
+    for(int i = mPos; i < (mPos + size_len); i++){
+        m[i] = size[i-mPos];
+    }
+    m[mPos + size_len] = ':';
+    mPos += size_len+1;
+
+    //Add filename to message
+    for(int i = mPos; i < mPos+strlen(p.filename); i++){
+        m[i] = p.filename[i-mPos];
+    }
+    m[mPos + strlen(p.filename)] = ':';
+    mPos += strlen(p.filename)+1;
+
+    //Add file data to message
+    for(int i = mPos; i < mSize; i++){
+        m[i] = p.filedata[i-mPos];
+    }
+
+    free(total_frag);
+    free(frag_no);
+    free(size);
+    
+    *message_len = mSize;
+    return m;
+}
+
+int main(int argc, char *argv[]){
+    int socketFD = socket(AF_INET,SOCK_DGRAM,0);
+    
+    //Check for 2 arguments
+    if (argc != 3){
+        printf("Incorrect number of arguments\n");
+        exit(1);
+    }
+
+    //Get arguments
+    int port = atoi(argv[2]);
+    char *ipAddress = argv[1];
+
+    char fileLoc[100];
+    char ftpCheck[10];
+    printf("Input a message of the following form: ftp <file name>\n");
+    //Check for ftp command
+    scanf("%s", &ftpCheck);
+    if(strcmp(ftpCheck, "ftp") != 0){
+        printf("Incorrect command\n");
+        exit(1);
+    }
+    //Get file location
+    scanf("%s", &fileLoc);
+
+    //Check if file exists
+    if(access(fileLoc, F_OK) != 0){
+        printf("That file does not exist\n");
+        exit(1);
+    }
+    
+    //read file + create packets/1000 byte
+    //check file size first
+    struct stat st;
+    stat(fileLoc, &st);
+    
+    
+    int total_frag = st.st_size/1000+1;
+    int frag_no=1;
+    
+    struct packet packet_array[7000]; //used to store packets we need to send
+    char temp_data[1000];
+    
+    int fd_from;  
+    size_t reader;
+    
+    FILE * fp;
+    fp = fopen(fileLoc, "rb");
+    
+    
+    //if((fd_from=open(fileLoc, O_RDONLY))<0)
+      //  printf("error reading file");   
+    while((reader=fread(packet_array[frag_no].filedata,1,1000,fp))>0 && frag_no<=total_frag){ 
+        packet_array[frag_no].total_frag = total_frag;
+        packet_array[frag_no].frag_no = frag_no;
+
+        packet_array[frag_no].filename = malloc(sizeof(fileLoc));
+        strcpy(packet_array[frag_no].filename,fileLoc);
+        printf("filename %s\n",packet_array[frag_no].filename);
+        if(frag_no!=total_frag)
+            packet_array[frag_no].size = 1000;
+        else
+            packet_array[frag_no].size = st.st_size%1000;
+
+        
+        //increment fragment number
+        frag_no++;
+    }
+    
+    //close file since we're done reading
+    if(fclose(fp)<0)
+        printf("error closing file");
+    
+    
+    
+    //Put server information into sockaddr_in
+    struct sockaddr_in sa;
+    memset(&sa.sin_zero, 0, 8*sizeof(unsigned char));
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(port);
+    inet_pton(AF_INET, ipAddress, &(sa.sin_addr));
+
+    ssize_t bytesSent;
+    
+    //continue ...
+    
+    
+    //Send ftp message to server
+    struct sockaddr_storage sa_stor;
+    socklen_t sa_stor_size = sizeof(sa_stor);
+    ssize_t bytesReceived;
+
+    int bufLen = 10;
+    char buf[bufLen];
+    
+    for(int i=1; i<frag_no;i++){
+        int length;
+        char* message_s = packet_to_message(packet_array[i],&length);
+        if((bytesSent = sendto(socketFD, message_s, length, 0, (struct sockaddr *) &sa, sizeof(sa))) == -1){
+            printf("Error sending message to server\n");
+            exit(1);
+        }
+        printf("waiting for ack\n");
+        if((bytesReceived = recvfrom(socketFD, buf, bufLen, 0, (struct sockaddr *) &sa_stor, &sa_stor_size)) == -1){
+            printf("Error receiving message from server");
+            exit(1);
+        }
+        printf("ack received\n");
+    }
+    
+
+    //Receive message from server
+    if((bytesReceived = recvfrom(socketFD, buf, bufLen, 0, (struct sockaddr *) &sa_stor, &sa_stor_size)) == -1){
+        printf("Error receiving message from server");
+        exit(1);
+    }
+    
+    
+    //Check return message from server
+    if(strcmp(buf, "completed") != 0){
+        exit(1);
+    }
+
+    printf("File transfer completed\n");
+    close(socketFD);
+    
+    
+    return 0;
+}
---
+++ lab1/sec3/server.c
@@ -0,0 +1,186 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <dirent.h>
+int server(char *port_num);
+
+struct packet {  
+    unsigned int total_frag;  
+    unsigned int frag_no; 
+    unsigned int size; 
+    char* filename; 
+    char filedata[1000];  
+};
+
+struct packet message_to_packet(char* m){
+    struct packet p;
+    
+    //Get total_frag
+    int start = 0;
+    int end = 0;
+    char val = m[end];
+    while(val != ':'){
+        end++;
+        val = m[end];
+    }
+    char total_frag[end-start+1];
+    memcpy(total_frag, &m[start], end);
+    total_frag[end-start] = '\0';
+    p.total_frag = atoi(total_frag);
+    end++;
+    start = end;
+    
+    //Get frag_no
+    val = m[end];
+    while(val != ':'){
+        end++;
+        val = m[end];
+    }
+    char frag_no[end-start+1];
+    memcpy(frag_no, &m[start], end);
+    frag_no[end-start] = '\0';
+    p.frag_no = atoi(frag_no);
+    end++;
+    start = end;
+    
+    //Get size
+    val = m[end];
+    while(val != ':'){
+        end++;
+        val = m[end];
+    }
+    char size[end-start+1];
+    memcpy(size, &m[start], end);
+    size[end-start] = '\0';
+    p.size = atoi(size);
+    end++;
+    start = end;
+    
+    //Get filename
+    val = m[end];
+    while(val != ':'){
+        end++;
+        val = m[end];
+    }
+    p.filename = malloc(end-start+1);
+    strncpy(p.filename, &m[start], end);
+    p.filename[end-start] = '\0';
+    end++;
+    start = end;
+    
+    //Get filedata
+    memcpy(p.filedata, &m[start], p.size);
+    
+    return p;
+}
+
+int main(int argc, char *argv[]){
+    
+    server(argv[1]);
+    
+    return 0;
+}
+
+int server(char *port_num){
+    int socket1 = socket(AF_INET,SOCK_DGRAM,0);
+    
+    struct sockaddr_storage deliver_addr;  //this will save the ip address of client
+    socklen_t addr_len;
+    int bytes_sent;
+    int bytes_received;
+    char buf[4000];
+    //code used in Beej's Guide to aqcuire IP
+    int status;
+    struct addrinfo hints;
+    struct addrinfo *servinfo;  // will point to the results
+
+    memset(&hints, 0, sizeof(hints)); // make sure the struct is empty
+    hints.ai_family = AF_INET;     // IPv4 
+    hints.ai_socktype = SOCK_DGRAM; // UDP stream sockets
+    hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+
+    if ((status = getaddrinfo(NULL, port_num, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        return -1;
+    }
+    //end of code taken from Beej
+    
+    if(bind(socket1, servinfo->ai_addr, servinfo->ai_addrlen) == -1){
+        //fail to bind
+        //close(socket1);
+        printf("failed to bind socket");
+        return -1;
+    }
+    
+    //server is set up and listening at port port_num...
+    printf("listener: waiting to recvfrom...\n");
+    
+    addr_len = sizeof(deliver_addr); 
+    if((bytes_received = recvfrom(socket1, buf, sizeof(buf), 0, (struct sockaddr *)&deliver_addr, &addr_len)) == -1){
+        //receive failed
+        printf("first listener:recvfrom failed");
+        return -1;
+    }
+    
+    
+    struct packet first_packet = message_to_packet(buf);
+    
+    int fd;
+    
+    
+    
+    char path[100]="received/";
+    strcat(path,first_packet.filename);
+    
+    FILE *fp = fopen(path,"ab");
+    
+    
+    size_t writer;
+    if((writer=fwrite(first_packet.filedata,1,first_packet.size,fp))<0)
+        printf("error writing file");
+    
+    char ack [10] = "yes";
+    if((bytes_sent = sendto(socket1,ack,sizeof(ack),0,(struct sockaddr *)&deliver_addr, addr_len))==-1){
+        //response to client failed
+        printf("listener:sendto failed");
+        return -1;
+    }
+    
+    for(int i=2; i<=first_packet.total_frag; i++){
+      
+        if((bytes_received = recvfrom(socket1, buf, sizeof(buf), 0, (struct sockaddr *)&deliver_addr, &addr_len)) == -1){
+            //receive failed
+            printf("listener:recvfrom failed");
+            return -1;
+        }
+        struct packet p = message_to_packet(buf);
+        if((writer=fwrite(p.filedata,1,p.size,fp))<0)
+            printf("error writing file");
+        
+        
+        if((bytes_sent = sendto(socket1,ack,sizeof(ack),0,(struct sockaddr *)&deliver_addr, addr_len))==-1){
+            //response to client failed
+            printf("listener:sendto failed");
+            return -1;
+        }
+    }
+    fclose(fp);
+    
+    
+    //respond to client
+    char message_return [10] = "completed";
+    if((bytes_sent = sendto(socket1,message_return,sizeof(message_return),0,(struct sockaddr *)&deliver_addr, addr_len))==-1){
+        //response to client failed
+        printf("listener:sendto failed");
+        return -1;
+    }
+    
+    //done
+    close(socket1);
+    return 0;
+}
